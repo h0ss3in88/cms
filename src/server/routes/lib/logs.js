3a1a69831d871110b11bdefe6e7043bdf67fa126
kaveh Taherian <kavehTaherian@hotmail.com>
2017-08-19 16:02:10 +0430

    fix syntax error for openshift

--- src/server/routes/lib/logs.js
+++ src/server/routes/lib/logs.js
@@ -7,7 +7,7 @@ var express = require('express'),
 module.exports = function () {
     _router
         .route('/account/users/profile/logs/:id')
-        .use(auth)
+        .use(auth())
         .param('id',function (req, res, next, id) {
             req.db.Log
                 .find({ 'user_id' : id })
